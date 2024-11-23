bab4860fc399dd4176563224a13f41b76127c0f2
mysd33 <mysd.33@gmail.com>
2024-11-23 18:41:37 +0900

    Repositoryの性能ログ、非同期実行依頼ログを出力するための対応

--- src/main/java/com/example/fw/common/async/repository/JobRequestRepositoryImpl.java
+++ src/main/java/com/example/fw/common/async/repository/JobRequestRepositoryImpl.java
@@ -4,25 +4,31 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.util.Assert;
 
 import com.example.fw.common.async.model.JobRequest;
+import com.example.fw.common.logging.ApplicationLogger;
+import com.example.fw.common.logging.LoggerFactory;
+import com.example.fw.common.message.CommonFrameworkMessageIds;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 
  * JobRequestRepositoryの実装クラス キューにSQSを使用し、JobRequestを登録する
  *
  */
-//@XRayEnabled
 @RequiredArgsConstructor
+@Slf4j
 public class JobRequestRepositoryImpl implements JobRequestRepository {
+	private static final ApplicationLogger appLogger = LoggerFactory.getApplicationLogger(log);
     private final JmsTemplate jmsTemplate;
     private final String queueName;
 
     @Override
     public void save(final JobRequest jobRequest) {
-        Assert.notNull(jobRequest, "jobRequestがNullです");
+        Assert.notNull(jobRequest, "jobRequestがNullです");        
         // キューに登録
         jmsTemplate.convertAndSend(queueName, jobRequest);
+        appLogger.info(CommonFrameworkMessageIds.I_CM_FW_0005, queueName, jobRequest);
     }
 
 }
--- src/main/java/com/example/fw/common/message/CommonFrameworkMessageIds.java
+++ src/main/java/com/example/fw/common/message/CommonFrameworkMessageIds.java
@@ -13,6 +13,7 @@ public final class CommonFrameworkMessageIds {
     public static final String I_CM_FW_0002 = "i.cm.fw.0002";
     public static final String I_CM_FW_0003 = "i.cm.fw.0003";
     public static final String I_CM_FW_0004 = "i.cm.fw.0004";
+    public static final String I_CM_FW_0005 = "i.cm.fw.0005";
     public static final String E_CM_FW_9001 = "e.cm.fw.9001";
     public static final String E_CM_FW_9002 = "e.cm.fw.9002";
 
