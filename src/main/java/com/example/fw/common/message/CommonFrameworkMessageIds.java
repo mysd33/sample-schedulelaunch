62e9ff1bf8ee41d22f921b02fce5f00c58ff8961
mysd33 <mysd.33@gmail.com>
2025-02-15 07:18:05 +0900

    DB動的ルーティングを出力するINFOログ対応

--- src/main/java/com/example/fw/common/message/CommonFrameworkMessageIds.java
+++ src/main/java/com/example/fw/common/message/CommonFrameworkMessageIds.java
@@ -14,6 +14,7 @@ public final class CommonFrameworkMessageIds {
     public static final String I_CM_FW_0003 = "i.cm.fw.0003";
     public static final String I_CM_FW_0004 = "i.cm.fw.0004";
     public static final String I_CM_FW_0005 = "i.cm.fw.0005";
+    public static final String I_CM_FW_0006 = "i.cm.fw.0006";
     public static final String E_CM_FW_9001 = "e.cm.fw.9001";
     public static final String E_CM_FW_9002 = "e.cm.fw.9002";
 
